fe6b2ec348742a9c968229f8f6f6b6f6524553b5
Angga Maulana <angga.mn@gmail.com>
2021-06-07 12:58:21 +0700

    add argparser for terminal execution

--- train.py
+++ train.py
@@ -3,27 +3,37 @@ import torch
 import torch.nn as nn
 import visdom
 import random
+import argparse
 
 from mcnn_model import MCNN
 from my_dataloader import CrowdDataset
 
 
 if __name__=="__main__":
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-novis", "--novis", required=False, help="Deactivate visdom", action="store_true")
+    ap.add_argument("-dataset", "--dataset", type=str, required=True, help="dataset root")
+    args = vars(ap.parse_args())
+
     torch.backends.cudnn.enabled=False
-    vis=visdom.Visdom()
+    vis = None
+    if args["novis"]==False:
+        vis=visdom.Visdom()
+
     device=torch.device("cuda")
     mcnn=MCNN().to(device)
     criterion=nn.MSELoss(size_average=False).to(device)
     optimizer = torch.optim.SGD(mcnn.parameters(), lr=1e-6,
                                 momentum=0.95)
     
-    img_root='D:\\workspaceMaZhenwei\\GithubProject\\MCNN-pytorch\\data\\Shanghai_part_A\\train_data\\images'
-    gt_dmap_root='D:\\workspaceMaZhenwei\\GithubProject\\MCNN-pytorch\\data\\Shanghai_part_A\\train_data\\ground_truth'
+    img_root = args["dataset"] + '/train_data/images'
+    gt_dmap_root = args["dataset"] + '/train_data/ground_truth'
     dataset=CrowdDataset(img_root,gt_dmap_root,4)
     dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=True)
 
-    test_img_root='D:\\workspaceMaZhenwei\\GithubProject\\MCNN-pytorch\\data\\Shanghai_part_A\\test_data\\images'
-    test_gt_dmap_root='D:\\workspaceMaZhenwei\\GithubProject\\MCNN-pytorch\\data\\Shanghai_part_A\\test_data\\ground_truth'
+    test_img_root = args["dataset"] + '/test_data/images'
+    test_gt_dmap_root = args["dataset"] + '/test_data/ground_truth'
     test_dataset=CrowdDataset(test_img_root,test_gt_dmap_root,4)
     test_dataloader=torch.utils.data.DataLoader(test_dataset,batch_size=1,shuffle=False)
 
@@ -69,18 +79,21 @@ if __name__=="__main__":
             min_epoch=epoch
         test_error_list.append(mae/len(test_dataloader))
         print("epoch:"+str(epoch)+" error:"+str(mae/len(test_dataloader))+" min_mae:"+str(min_mae)+" min_epoch:"+str(min_epoch))
-        vis.line(win=1,X=epoch_list, Y=train_loss_list, opts=dict(title='train_loss'))
-        vis.line(win=2,X=epoch_list, Y=test_error_list, opts=dict(title='test_error'))
+        if vis is not None:
+            vis.line(win=1,X=epoch_list, Y=train_loss_list, opts=dict(title='train_loss'))
+            vis.line(win=2,X=epoch_list, Y=test_error_list, opts=dict(title='test_error'))
         # show an image
         index=random.randint(0,len(test_dataloader)-1)
         img,gt_dmap=test_dataset[index]
-        vis.image(win=3,img=img,opts=dict(title='img'))
-        vis.image(win=4,img=gt_dmap/(gt_dmap.max())*255,opts=dict(title='gt_dmap('+str(gt_dmap.sum())+')'))
+        if vis is not None:
+            vis.image(win=3,img=img,opts=dict(title='img'))
+            vis.image(win=4,img=gt_dmap/(gt_dmap.max())*255,opts=dict(title='gt_dmap('+str(gt_dmap.sum())+')'))
         img=img.unsqueeze(0).to(device)
         gt_dmap=gt_dmap.unsqueeze(0)
         et_dmap=mcnn(img)
         et_dmap=et_dmap.squeeze(0).detach().cpu().numpy()
-        vis.image(win=5,img=et_dmap/(et_dmap.max())*255,opts=dict(title='et_dmap('+str(et_dmap.sum())+')'))
+        if vis is not None:
+            vis.image(win=5,img=et_dmap/(et_dmap.max())*255,opts=dict(title='et_dmap('+str(et_dmap.sum())+')'))
         
 
 
